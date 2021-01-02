7ab0bea2f2cf57da1a57a8553e2dc353f3089ca3
UdayPulickal <u_dayan@yahoo.co.in>
2021-01-02 20:10:56 +0000

    2 files modified

--- src/app/employeeList.component.ts
+++ src/app/employeeList.component.ts
@@ -5,34 +5,51 @@ import { Component } from "@angular/core";
   styleUrls: ["employeeList.component.css"]
 })
 export class EmployeeListComponent {
-  employees: any[] = [
-    {
-      code: "emp101",
-      name: "Tom",
-      gender: "Male",
-      annualSalary: 5500,
-      dateOfBirth: "25/6/1988"
-    },
-    {
-      code: "emp102",
-      name: "Alex",
-      gender: "Male",
-      annualSalary: 5700.95,
-      dateOfBirth: "9/6/1982"
-    },
-    {
-      code: "emp103",
-      name: "Mike",
-      gender: "Male",
-      annualSalary: 5900,
-      dateOfBirth: "12/8/1979"
-    },
-    {
-      code: "emp104",
-      name: "Mary",
-      gender: "Female",
-      annualSalary: 6500.826,
-      dateOfBirth: "14/10/1980"
-    }
-  ];
+  employees: any[];
+  constructor() {
+    this.employees = [
+      {
+        code: "emp101",
+        name: "Tom",
+        gender: "Male",
+        annualSalary: 5500,
+        dateOfBirth: "25/6/1988"
+      },
+      {
+        code: "emp102",
+        name: "Alex",
+        gender: "Male",
+        annualSalary: 5700.95,
+        dateOfBirth: "9/6/1982"
+      },
+      {
+        code: "emp103",
+        name: "Mike",
+        gender: "Male",
+        annualSalary: 5900,
+        dateOfBirth: "12/8/1979"
+      },
+      {
+        code: "emp104",
+        name: "Mary",
+        gender: "Female",
+        annualSalary: 6500.826,
+        dateOfBirth: "14/10/1980"
+      }
+    ];
+  }
+  getemployees(): void {
+    this.employees = [
+      {
+        code: "emp105",
+        name: "Mary Kom",
+        gender: "Female",
+        annualSalary: 6500.826,
+        dateOfBirth: "14/10/1980"
+      }
+    ];
+  }
+  trackByempCode(index: number, employee: any): string {
+    return employee.code;
+  }
 }
